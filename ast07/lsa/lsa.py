08a707757badf1262ce22dc24117ffb05db7538c
ethanuo <ethan.ubando@cambrianacademy.org>
2022-07-26 15:57:13 +0000

    Fixed critical bug

--- ast07/lsa/lsa.py
+++ ast07/lsa/lsa.py
@@ -41,7 +41,7 @@ full_file_list = os.listdir('../full') # creates a Python list containing the fi
 
 # |%%--%%| <8X5ZoLSZ5N|YN09D7F4yY>
 # Cool way to sort array in only one line
-df = pd.read_csv('csv/all_categ_vals.csv')
+df = pd.read_csv('../csv/all_categ_vals.csv')
 all_categ_vals = df.values.tolist()
 
 #Get indicies for value set
@@ -49,9 +49,9 @@ three_object_indexes = [w for w,x,y,z in all_categ_vals if (x==3 and y == 3 and 
 
 # |%%--%%| <YN09D7F4yY|I219QxkTi5>
 
-all_mags_narrow = pd.read_csv('csv/all_mags_narrow.csv')#.values.tolist()
-all_mjd_narrow = pd.read_csv('csv/all_mjd_narrow.csv')#.values.tolist()
-all_magerrs_narrow = pd.read_csv('csv/all_magerrs_narrow.csv')#.values.tolist()
+all_mags_narrow = pd.read_csv('../csv/all_mags_narrow.csv')#.values.tolist()
+all_mjd_narrow = pd.read_csv('../csv/all_mjd_narrow.csv')#.values.tolist()
+all_magerrs_narrow = pd.read_csv('../csv/all_magerrs_narrow.csv')#.values.tolist()
 all_mags_narrow.drop('Unnamed: 0',inplace=True,axis=1)
 all_mjd_narrow.drop('Unnamed: 0',inplace=True,axis=1)
 all_magerrs_narrow.drop('Unnamed: 0',inplace=True,axis=1)
@@ -110,41 +110,22 @@ def lomb_scargle_list (object_list):
     return max_over_mean,object_list
 
 #|%%--%%| <XUbQvcqmNO|xSkcEvJdom>
-print('lsa')
 max_over_mean_threes,object_list = lomb_scargle_list([i for i in range(397)])
-print('lsa done')
-print(object_list_saved)
-max_over_mean_threes_saved = max_over_mean_threes
-object_list_saved = object_list
+#max_over_mean_threes_saved = max_over_mean_threes
+#object_list_saved = object_list
 #|%%--%%| <xSkcEvJdom|ZKXubZNHmX>
 #print(max_over_mean_threes)
 narrowed_objects = []
 for i in range(len(max_over_mean_threes_saved)):
-    if max_over_mean_threes_saved[i] >= 20:
-        narrowed_objects.append([object_list_saved[i],max_over_mean_threes_saved[i]])
-
-
-len(narrowed_objects)
+    if max_over_mean_threes[i] >= 20:
+        narrowed_objects.append([object_list[i],max_over_mean_threes[i]])
 
 #|%%--%%| <ZKXubZNHmX|5hvgRVGBTX>
 import csv
-#dfx = pd.DataFrame(narrowed_objects)
 with open('blahblah', 'w', newline='') as blah:
     wr = csv.writer(blah, quoting=csv.QUOTE_ALL)
     wr.writerow(narrowed_objects)
 
 
 #|%%--%%| <5hvgRVGBTX|GcNKluNsHb>
-df_x = pd.DataFrame(narrowed_objects)
-import base64
-import pandas as pd
-from IPython.display import HTML
-def create_download_link( df, title = "Download CSV file", filename = "data.csv"):
-    csv = df.to_csv()
-    b64 = base64.b64encode(csv.encode())
-    payload = b64.decode()
-    html = '<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>'
-    html = html.format(payload=payload,title=title,filename=filename)
-    return HTML(html)
-create_download_link(df_x)
 
